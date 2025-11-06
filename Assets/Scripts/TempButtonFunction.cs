571d3777709f3d30434815ddd51fed5640d9dec7
barath-balamurugan <barath0812@gmail.com>
2025-11-06 17:29:21 -0500

    Implemented the temperature setup

---
+++ Assets/Scripts/TempButtonFunction.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+using System.Collections;
+
+public class TempButtonFunction : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+{
+    [Header("Target")]
+    public TMP_Text valueText;
+
+    [Header("Behavior")]
+    public bool isIncrement = true;
+    public int step = 5;
+    public float minValue = 0f;
+    public float maxValue = 1000f;
+
+    [Header("Long Press")]
+    public float longPressDelay = 0.35f;
+    public float repeatInterval = 0.07f;
+
+    bool holding;
+    Coroutine repeatCo;
+
+    public void ClickOnce() => ApplyDelta();
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        holding = true;
+        ApplyDelta();
+        repeatCo = StartCoroutine(HoldRepeat());
+    }
+
+    public void OnPointerUp(PointerEventData eventData) => StopHold();
+    public void OnPointerExit(PointerEventData eventData) => StopHold();
+
+    void StopHold()
+    {
+        holding = false;
+        if (repeatCo != null) { StopCoroutine(repeatCo); repeatCo = null; }
+    }
+
+    IEnumerator HoldRepeat()
+    {
+        yield return new WaitForSeconds(longPressDelay);
+        while (holding)
+        {
+            ApplyDelta();
+            yield return new WaitForSeconds(repeatInterval);
+        }
+    }
+
+    void ApplyDelta()
+    {
+        if (!valueText) return;
+
+        if (!int.TryParse(valueText.text, out int val))
+            val = 0;
+
+        val += isIncrement ? step : -step;
+        val = Mathf.Clamp(val, minValue, maxValue);
+        valueText.text = val.ToString();
+    }
+}
